c59cbccb970ab7550f58d6adaef511c896d7a716
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-08 19:06:59 +0000

    windi

--- windi.config.ts
+++ windi.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite-plugin-windicss'
 
 export default defineConfig({
     darkMode: 'class',
+    extract: {
+        include: ['src/**/*.{vue,html,jsx,tsx}'],
+        exclude: ['node_modules', '.git'],
+  },
 });
